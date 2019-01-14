4d0ed2083a2c37d4e74d7e5debbdce2617791f41
Mario Serrano <maseflores79@gmail.com>
2019-01-14 14:15:24 -0800

    Organizando los tests con describe()

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,35 +1,39 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-// Behavior Driven development
-it('should add two numbers', ( ) => {
-    var res = utils.add(33,11);
-    expect(res).toBe(44).toBeA('number');
-});
+describe('Utils tests', () => {
+    // Behavior Driven development
+    it('should add two numbers', () => {
+        var res = utils.add(33, 11);
+        expect(res).toBe(44).toBeA('number');
+    });
 
-it('should asyncAdd two numbers', (done)=>{
-    utils.asyncAdd(4,3, (sum)=>{
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    it('should asyncAdd two numbers', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
+    });
+
+    it('should square one number', () => {
+        var res = utils.square(5);
+        expect(res).toBe(25).toBeA('number');
+    });
+
+    it('should asyncSquare one number', (done) => {
+        utils.asyncSquare(4, (sq) => {
+            expect(sq).toBe(16).toBeA('number');
+            done();
+        });
+    });
+
+    it('should verify first and last names are set', () => {
+        var usuario = utils.setName({ firstname: '', lastname: '' }, 'Mario Serrano');
+        expect(usuario).toInclude({ firstname: 'Mario', lastname: 'Serrano' });
     });
 });
 
-it('should square one number', ( ) => {
-    var res = utils.square(5);
-    expect(res).toBe(25).toBeA('number');
-});
 
-it('should asyncSquare one number', (done)=>{
-    utils.asyncSquare(4, (sq)=>{
-        expect(sq).toBe(16).toBeA('number');
-        done();
-    });
-});
-
-it('should verify first and last names are set', ()=>{
-    var usuario = utils.setName( {firstname: '', lastname: ''}, 'Mario Serrano' );
-    expect(usuario).toInclude({firstname: 'Mario', lastname: 'Serrano'});
-});
 /*
 it ('should expect some values', () => {
   // expect(12).toNotBe(11);
